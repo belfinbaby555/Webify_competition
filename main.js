b52a659717077ce76f579c2bd896f47596303404
AshishJoseph <87479500+1blackghost@users.noreply.github.com>
2023-01-22 01:12:36 +0530

    done main.js edit and transition

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function gallary(imgs) {
     var expandImg = document.getElementById("expandedImg");
     expandImg.src = imgs.src;
+    document.getElementById(imgs.id).style.transform='scale(1.1) translateY(10px)'
     expandImg.parentElement.style.display = "block";
+    function func(){
+      document.getElementById(imgs.id).style.transform='scale(1) translateY(0px)'
+
+    }
+    setTimeout(func,2000);
   }
